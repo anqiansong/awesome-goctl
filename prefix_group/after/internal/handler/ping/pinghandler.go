41f54702773516a28acf0990992c91c7d929c3c7
anqiansong <anqiansong@gmail.com>
2022-07-04 23:12:09 +0800

    Add prefix & group

---
+++ prefix_group/after/internal/handler/ping/pinghandler.go
@@ -0,0 +1,21 @@
+package ping
+
+import (
+	"net/http"
+
+	"after/internal/logic/ping"
+	"after/internal/svc"
+	"github.com/zeromicro/go-zero/rest/httpx"
+)
+
+func PingHandler(svcCtx *svc.ServiceContext) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		l := ping.NewPingLogic(r.Context(), svcCtx)
+		err := l.Ping()
+		if err != nil {
+			httpx.Error(w, err)
+		} else {
+			httpx.Ok(w)
+		}
+	}
+}
